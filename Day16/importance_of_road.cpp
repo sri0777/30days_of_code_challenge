1a1ce2083e86988176cce3780859d3ef62afa91d
Srijan <srijan.hard.1234@gmail.com>
2024-06-28 23:52:33 +0530

    graph

---
+++ Day16/importance_of_road.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        map<int,int> mp;
+        vector<int> degree(n,0);
+        for(auto vec : roads){ //indegree for bidirectional
+            int u = vec[0];
+            int v = vec[1];
+
+            degree[u]++;
+            degree[v]++;
+        }
+        sort(degree.begin(),degree.end());
+        long long value = 1;
+        long long sum = 0;
+        for(int i=0; i<n; i++){
+            sum+= degree[i] * value;
+            value++;
+        }
+
+        return sum;
+    }
+};
